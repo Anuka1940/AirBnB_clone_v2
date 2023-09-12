68f2b8c2caa7aa1ad292d9f598533e96e01fb2aa
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-09-12 15:15:41 -0700

    fix pycodestyle error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" A fabric scrip that generates a .tgz archive 
+""" A fabric scrip that generates a .tgz archive
 from the contents of the web_static folder using the do_pack function"""
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     try:
@@ -16,7 +17,7 @@ def do_pack():
         # Compress the contents of the "web_static" folder into the archive
         local(f"tar -czvf versions/{archive_name} web_static")
 
-        #Return the path to the created archive
+        # Return the path to the created archive
         return f"versions/{archive_name}"
     except Exception:
         return None

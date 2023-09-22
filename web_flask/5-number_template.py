fc8fea5c346ccaaefeb59d502167696711f600e1
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-09-21 18:10:58 -0700

    task-5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""this module concatinates two strings"""
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """returns hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """returns c and text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_text(text="is cool"):
+    """returns python and text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """returns number and n"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_n(n):
+    """returns number and n"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
